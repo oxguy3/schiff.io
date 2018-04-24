4778a321acdad6e1ee4c38340cfbef0eaf236786
Hayden Schiff <haydenschiff@gmail.com>
2018-04-24 13:14:03 -0400

    xss demo: register form now remembers your input

--- unsafe.schiff.io/xss-presentation-demos/widgets-register.php
+++ unsafe.schiff.io/xss-presentation-demos/widgets-register.php
@@ -1,11 +1,27 @@
 <?php
 require_once('widgets-common.php');
 
+function printSavedFormValue($field) {
+    $value = false;
+    if (isset($_POST[$field])) {
+        $value = $_POST[$field];
+    } else if (isset($_COOKIE['form_'.$field])) {
+        $value = $_COOKIE['form_'.$field];
+    }
+    if ($value !== false) {
+        echo " value=\"".htmlspecialchars($value, ENT_QUOTES | ENT_HTML5)."\"";
+    }
+}
+
 $db = getPDO();
 
 $insertId = false;
 
 if (isset($_POST['register'])) {
+    $formFields = ['email','password','nameFirst','nameMiddle','nameLast','phone','twitter','imageUrl'];
+    foreach ($formFields as $field) {
+        setcookie('form_'.$field, $_POST[$field]);
+    }
     $insertSql = 'INSERT INTO registrations(email, password, nameFirst, nameMiddle, nameLast, phone, twitter, imageUrl) VALUES(:email, :password, :nameFirst, :nameMiddle, :nameLast, :phone, :twitter, :imageUrl)';
     $stmt = $db->prepare($insertSql);
     $stmt->execute([
@@ -33,31 +49,31 @@ printHead('Register your account');
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="registerInputEmail">Email address*</label>
-                            <input type="email" name="text" class="form-control" id="registerInputEmail" placeholder="hjfarnsworth@planetexpress.co" required>
+                            <input type="email" name="email" class="form-control" id="registerInputEmail" placeholder="hjfarnsworth@planetexpress.co" required<?php printSavedFormValue('email'); ?>>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="registerInputPassword">Password*</label>
-                            <input type="password" name="password" class="form-control" id="registerInputPassword" placeholder="hunter2" required>
+                            <input type="password" name="password" class="form-control" id="registerInputPassword" placeholder="hunter2" required<?php printSavedFormValue('password'); ?>>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-5">
-                            <label for="registerInputFirstName">First name</label>
-                            <input type="text" name="nameFirst" class="form-control" id="registerInputFirstName" placeholder="Hubert">
+                            <label for="registerInputFirstName">First name*</label>
+                            <input type="text" name="nameFirst" class="form-control" id="registerInputFirstName" placeholder="Hubert" required<?php printSavedFormValue('nameFirst'); ?>>
                         </div>
                         <div class="form-group col-md-2">
                             <label for="registerInputMiddleInitial">M.I.</label>
-                            <input type="text" name="nameMiddle" class="form-control" id="registerInputMiddleInitial" placeholder="J">
+                            <input type="text" name="nameMiddle" class="form-control" id="registerInputMiddleInitial" placeholder="J"<?php printSavedFormValue('nameMiddle'); ?>>
                         </div>
                         <div class="form-group col-md-5">
-                            <label for="registerInputLastName">Last name</label>
-                            <input type="text" name="nameLast" class="form-control" id="registerInputLastName" placeholder="Farnsworth">
+                            <label for="registerInputLastName">Last name*</label>
+                            <input type="text" name="nameLast" class="form-control" id="registerInputLastName" placeholder="Farnsworth" required<?php printSavedFormValue('nameLast'); ?>>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="registerInputPhone">Phone</label>
-                            <input type="tel" name="phone" class="form-control" id="registerInputPhone" placeholder="513-867-5309">
+                            <input type="tel" name="phone" class="form-control" id="registerInputPhone" placeholder="513-867-5309"<?php printSavedFormValue('phone'); ?>>
                         </div>
                         <div class="form-group col-md-4">
                             <label for="registerInputTwitter">Twitter username</label>
@@ -65,12 +81,12 @@ printHead('Register your account');
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">@</span>
                                 </div>
-                                <input type="text" name="twitter" class="form-control" id="registerInputTwitter" placeholder="prof_farnsworth">
+                                <input type="text" name="twitter" class="form-control" id="registerInputTwitter" placeholder="prof_farnsworth"<?php printSavedFormValue('twitter'); ?>>
                             </div>
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="registerInputImageURL">Profile picture URL <small>(32x32 pixels)</small></label>
-                            <input type="text" name="imageUrl" class="form-control" id="registerInputImageURL" placeholder="https://i.imgur.com/R8IFHAB.jpg">
+                            <label for="registerInputImageURL">Profile picture URL <small>(32x32)</small></label>
+                            <input type="text" name="imageUrl" class="form-control" id="registerInputImageURL" placeholder="https://i.imgur.com/R8IFHAB.jpg"<?php printSavedFormValue('imageUrl'); ?>>
                         </div>
                     </div>
                     <button type="submit" name="register" class="btn btn-primary">Submit</button>
